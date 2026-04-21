a508cd7f277bc04dfe7ece5f38881b079f5a5951
RyanParhizi <ryan.parhizi@gmail.com>
2026-04-21 00:24:15 -0600

    implemented uart.v and the python script to transfer data from a pc

--- source/DisplayHDMI.v
+++ source/DisplayHDMI.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/14/2026 04:48:34 PM
-// Design Name: 
-// Module Name: DisplayHDMI
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module DisplayHDMI(
     input wire clk,
